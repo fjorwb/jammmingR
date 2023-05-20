66ddcb75b419dca991fd82d6f239518d22e49323
FJORWB <88600252+fjorwb@users.noreply.github.com>
2023-05-20 00:48:56 -0500

    boilerplate setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import './App.css'
 
-import Comp1 from './components/comp1'
-import Comp2 from './components/comp2'
-
 function App () {
   return (
     <div className='App'>
       <h1>Jammming</h1>
-      <Comp1 />
-      <Comp2 />
     </div>
   )
 }
